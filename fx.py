10ef61afd91e8264e911b9335f6a0feb5716059e
ViraZ <zhykvira@ukr.net>
2020-08-02 17:30:16 +0300

    Commit

--- fx.py
+++ fx.py
@@ -1,26 +1,13 @@
-import scrapy
-from urllib.parse import urlparse
+from selenium import webdriver
 
+with webdriver.Firefox() as driver:
+    driver.get('http://localhost:8985')
 
-class FXSpider(scrapy.Spider):
-    name = 'fx'
-    start_urls = [
-        'http://localhost:8987'
-    ]
+    date = driver.find_element_by_css_selector('.date').text
+    print(f'FX rates for {date}')
 
-    def parse(self, response):
-        for a in response.css('li.list-group-item a::attr(href)'):
-            yield response.follow(a.get(), callback=self.parse_date)
-
-    def parse_date(self, response):
-        path = urlparse(response.url).path
-        # /2019-11-03
-        data = {'day': path[1:]}
-        for tr in response.css('tr'):
-            name_td, price_td = tr.css('td')
-            # <i ... title="GBP">
-            symbol = name_td.css('i::attr(title)').get()
-            # <td>0.84</td>
-            ratio = float(price_td.css('::text').get())
-            data[symbol] = ratio
-        yield data
+    tds = driver.find_elements_by_tag_name('td')
+    for i in range(0, len(tds), 2):
+        symbol = tds[i].text
+        ratio = float(tds[i+1].text)
+        print(f'- {symbol} -> {ratio:.2f}')
--- httpd.py
+++ httpd.py
@@ -1,71 +1,35 @@
-from datetime import datetime, timedelta
+import json
 from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
-from io import StringIO
 from pathlib import Path
 from urllib.parse import urlparse
-import re
 
 here = Path(__file__).absolute().parent
-start_date = datetime(2019, 11, 1)
-num_days = 8
-start_prices = [
-    ('GBP', 0.83),
-    ('EUR', 0.89),
-    ('JPY', 105.3),
-    ('BTC', 0.01),
-    ('ILS', 3.48),
-]
-time_fmt = '%Y-%m-%d'
 
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         url = urlparse(self.path)
         if url.path == '/':
-            html = self.index()
+            response = self.load_html('fx.html')
+            content_type = 'text/html'
         else:
-            try:
-                # /2019-11-02
-                date = datetime.strptime(url.path[1:], time_fmt)
-            except ValueError:
-                self.send_error(HTTPStatus.NOT_FOUND)
-                return
-            diff = (date - start_date).days / 100
-            args = {
-                symbol: price + diff for symbol, price in start_prices
-            }
-            args['date'] = date.strftime(time_fmt)
-
-            template = self.load_html('day.html')
-
-            def replace(match):
-                val = args[match.group(1)]
-                if isinstance(val, float):
-                    val = f'{val:.2f}'
-                return val
-            html = re.sub('{{([a-zA-Z]+)}}', replace, template)
+            response = json.dumps({
+                'ratios': [
+                    {'symbol': 'GBP', 'ratio': 0.83},
+                    {'symbol': 'EUR', 'ratio': 0.89},
+                    {'symbol': 'JPY', 'ratio': 105.3},
+                    {'symbol': 'BTC', 'ratio': 0.01},
+                    {'symbol': 'ILS', 'ratio': 3.48},
+                ],
+                'date': '2019-11-11',
+            })
+            content_type = 'application/json'
 
         self.send_response(HTTPStatus.OK)
-        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Type', content_type)
         self.end_headers()
-        self.wfile.write(html.encode('utf-8'))
-
-    def index(self):
-        io = StringIO()
-        print('<ul class="list-group">', file=io)
-        for i in range(num_days):
-            day = start_date + timedelta(days=i)
-            day = day.strftime(time_fmt)
-            li = (
-                '\t<li class="list-group-item">'
-                f'<a href="/{day}">{day}</a></li>'
-            )
-            print(li, file=io)
-        print('</ul>', file=io)
-
-        dates = io.getvalue()
-        return self.load_html('index.html').format(dates=dates)
+        self.wfile.write(response.encode('utf-8'))
 
     def load_html(self, name):
         path = here / name
@@ -74,7 +38,7 @@ class Handler(BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    host, port = 'localhost', 8987
+    host, port = 'localhost', 8985
     server = ThreadingHTTPServer((host, port), Handler)
     print(f'server ready on {host}:{port}')
     server.serve_forever()
